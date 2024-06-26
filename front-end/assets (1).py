891ccb48e009951df848282fedcdf5e0b2cc75d1
UmerFq56 <141590838+UmerFq56@users.noreply.github.com>
2024-06-25 23:45:36 -0400

    Update assets (1).py

--- front-end/assets (1).py
+++ front-end/assets (1).py
@@ -19,11 +19,6 @@ visual aspects of the car recommendation tool.
 Copyright and Usage Information
 ===============================
 
-This file is provided solely for the personal and private use of students
-taking CSC111 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited.
-
 This file is Copyright (c) 2024 CSC111 Students Winter (Yaseen Sadat, Muhammad Aneeq, Umer Farooqui, Zarif Ali)
 
 """
